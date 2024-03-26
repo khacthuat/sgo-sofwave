9eddc1fdf20a6ecd20c5ec6ff15a0243eaed653a
youngbachhh <dippercode.01@gmail.com>
2024-03-26 17:32:54 +0700

    add: crm laravel

--- database/migrations/2024_03_26_080935_create_sales_table.php
+++ database/migrations/2024_03_26_080935_create_sales_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('phone_number');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('sales');
     }
 };
