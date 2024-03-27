2e3e75663f1608a68a5ae44d18f914afd2c06d7d
youngbachhh <dippercode.01@gmail.com>
2024-03-27 07:36:22 +0700

    change data type

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
     public function index()
     {
         //
-        return response()->json(User::all());
+        return User::all();
     }
 
     /**
